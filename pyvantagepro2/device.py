4dca86e3f9f31146de84e2864107e3dd0b5fff43
moparman70 <58913167+moparman70@users.noreply.github.com>
2025-11-13 17:16:25 -0500

    Update device.py

--- pyvantagepro2/device.py
+++ pyvantagepro2/device.py
@@ -1,25 +1,31 @@
-"""pyvantagepro2.device-------------------Allows data query of Davis Vantage Pro2 devices."""
+"""
+pyvantagepro2.device
+-------------------
 
-from __future__ import annotations
+Allows data query of Davis Vantage Pro2 devices
 
-from datetime import datetime, timedelta
+
+"""
+
+from __future__ import division, unicode_literals
 import struct
+from datetime import datetime, timedelta
+from pylink import link_from_url
 
-from aioretry import RetryInfo, RetryPolicyStrategy, retry
-import aioserial
-from async_property import async_cached_property
+from .logger import LOGGER
+from .utils import cached_property, retry, bytes_to_hex, ListDict, is_bytes
 
 from .parser import (
-    ArchiveDataParserRevB,
+    LoopDataParserRevB,
+    HighLowParserRevB,
     DmpHeaderParser,
     DmpPageParser,
-    LoopDataParserRevB,
+    ArchiveDataParserRevB,
     VantageProCRC,
     pack_datetime,
-    pack_dmp_date_time,
     unpack_datetime,
+    pack_dmp_date_time,
 )
-from .utils import ListDict, is_bytes
 
 
 class NoDeviceException(Exception):
@@ -32,8 +38,6 @@ class BadAckException(Exception):
     """No valid acknowledgement."""
 
     def __str__(self):
-        """Return status."""
-
         return self.__doc__
 
 
@@ -41,8 +45,6 @@ class BadCRCException(Exception):
     """No valid checksum."""
 
     def __str__(self):
-        """Return status."""
-
         return self.__doc__
 
 
@@ -50,19 +52,12 @@ class BadDataException(Exception):
     """No valid data."""
 
     def __str__(self):
-        """Return status."""
-
         return self.__doc__
 
 
-def retry_policy(info: RetryInfo) -> RetryPolicyStrategy:
-    """Retry policy."""
-
-    return info.fails > 3, 1
-
-
 class VantagePro2:
-    """Communicates with the station by sending commands, reads the binary data and parsing it into usable scalar values.
+    """Communicates with the station by sending commands, reads the binary
+    data and parsing it into usable scalar values.
 
     :param link: A `PyLink` connection.
     """
@@ -77,37 +72,40 @@ class VantagePro2:
     ESC = "\x1b"
     OK = "\n\rOK\n\r"
 
-    def __init__(self, link: aioserial.AioSerial) -> None:
-        """Initialize some shit."""
-
+    def __init__(self, link):
         self.link = link
-        # self._check_revision()
-        self.RevA = False
-        self.RevB = True
+        self.link.open()
+        self._check_revision()
 
     @classmethod
-    async def from_url(cls, url, timeout=10):
-        """Get device from url."""
+    def from_url(cls, url, timeout=10):
+        """Get device from url.
 
-        link = aioserial.AioSerial(port=url, baudrate=115200, timeout=timeout)
+        :param url: A `PyLink` connection URL.
+        :param timeout: Set a read timeout value.
+        """
+        link = link_from_url(url)
+        link.settimeout(timeout)
         return cls(link)
 
-    @retry(retry_policy)
-    async def wake_up(self) -> bool:
+    @retry(tries=3, delay=1)
+    def wake_up(self):
         """Wakeup the station console."""
-
         wait_ack = self.WAKE_ACK
-        await self.link.write_async(self.WAKE_STR.encode())
-        ack = await self.link.read_async(len(wait_ack))
-        if wait_ack == ack.decode():
+        #        LOGGER.info("try wake up console")
+        self.link.write(self.WAKE_STR)
+        ack = self.link.read(len(wait_ack))
+        if wait_ack == ack:
+            #            LOGGER.info("Check ACK: OK (%s)" % (repr(ack)))
             return True
         # Sometimes we have a 1byte shift from Vantage Pro and that's why wake up doesn't work anymore
         # We just shift another 1byte to be aligned in the serial buffer again.
-        await self.link.read_async(1)
-        raise NoDeviceException
+        self.link.read(1)
+        #        LOGGER.error("Check ACK: BAD (%s != %s)" % (repr(wait_ack), repr(ack)))
+        raise NoDeviceException()
 
-    @retry(retry_policy)
-    async def send(self, data, wait_ack=None, timeout=None):
+    @retry(tries=3, delay=0.5)
+    def send(self, data, wait_ack=None, timeout=None):
         """Sends data to station.
 
         :param data: Can be a byte array or an ASCII command. If this is
@@ -118,63 +116,79 @@ class VantagePro2:
 
         :param timeout: Define this timeout when reading ACK from link﻿.
         """
-        if await is_bytes(data):
-            await self.link.write_async(data)
+        if is_bytes(data):
+            #            LOGGER.info("try send : %s" % bytes_to_hex(data))
+            self.link.write(data)
         else:
-            await self.link.write_async((f"{data}\n").encode())
+            #            LOGGER.info("try send : %s" % data)
+            self.link.write("%s\n" % data)
         if wait_ack is None:
             return True
-        ack = await self.link.read_async(len(wait_ack))
+        ack = self.link.read(len(wait_ack), timeout=timeout)
         if wait_ack == ack:
+            #            LOGGER.info("Check ACK: OK (%s)" % (repr(ack)))
             return True
-        raise BadAckException
+        #        LOGGER.error("Check ACK: BAD (%s != %s)" % (repr(wait_ack), repr(ack)))
+        raise BadAckException()
 
-    @retry(retry_policy)
-    async def read_from_eeprom(self, hex_address, size):
-        """Reads from EEPROM the `size` number of bytes starting at the `hex_address`. Results are given as hex strings."""
-
-        await self.link.write_async(
-            (f"{'EEBRD '}{hex_address}{' '}{size:02d}\n").encode()
-        )
-        ack = await self.link.read_async(len(self.ACK))
-        if ack.decode() == self.ACK:
-            data = await self.link.read_async(size + 2)  # 2 bytes for CRC
+    @retry(tries=3, delay=1)
+    def read_from_eeprom(self, hex_address, size):
+        """Reads from EEPROM the `size` number of bytes starting at the
+        `hex_address`. Results are given as hex strings."""
+        self.link.write("EEBRD %s %.2d\n" % (hex_address, size))
+        ack = self.link.read(len(self.ACK))
+        if self.ACK == ack:
+            #            LOGGER.info("Check ACK: OK (%s)" % (repr(ack)))
+            data = self.link.read(size + 2)  # 2 bytes for CRC
             if VantageProCRC(data).check():
                 return data[:-2]
-            raise BadCRCException
-        raise BadAckException
+            else:
+                raise BadCRCException()
+        else:
+            msg = "Check ACK: BAD (%s != %s)" % (repr(self.ACK), repr(ack))
+            #            LOGGER.error(msg)
+            raise BadAckException()
 
-    async def gettime(self):
+    def gettime(self):
         """Returns the current datetime of the console."""
-
-        await self.wake_up()
-        await self.send("GETTIME", self.ACK)
-        data = await self.link.read_async(8)
+        self.wake_up()
+        self.send("GETTIME", self.ACK)
+        data = self.link.read(8)
         if isinstance(data, bytes):
             return unpack_datetime(data)
-        return None
 
-    async def settime(self, dtime):
+    def settime(self, dtime):
         """Set the given `dtime` on the station."""
+        self.wake_up()
+        self.send("SETTIME", self.ACK)
+        self.send(pack_datetime(dtime), self.ACK)
 
-        await self.wake_up()
-        await self.send("SETTIME", self.ACK)
-        await self.send(pack_datetime(dtime), self.ACK)
-
-    async def get_current_data(self):
+    def get_current_data(self):
         """Returns the real-time data as a `Dict`."""
-
-        await self.wake_up()
-        await self.send("LOOP 1", self.ACK)
-        current_data = await self.link.read_async(99)
+        self.wake_up()
+        self.send("LOOP 1", self.ACK)
+        current_data = self.link.read(99)
         if isinstance(current_data, bytes):
             if self.RevB:
                 return LoopDataParserRevB(current_data, datetime.now())
             raise NotImplementedError("Do not support RevB data format")
         return None
 
-    async def get_archives(self, start_date=None, stop_date=None):
-        """Get archive records until `start_date` and `stop_date` as ListDict.
+    def get_hi_lows(self):
+        """Get high/low data."""
+
+        self.wake_up()
+        self.send("HILOWS", self.ACK)
+        current_data = self.link.read(436)
+        if isinstance(current_data, bytes):
+            if self.RevB:
+                return HighLowParserRevB(current_data)
+            raise NotImplementedError("Do not support RevB data format")
+        return None
+
+    def get_archives(self, start_date=None, stop_date=None):
+        """Get archive records until `start_date` and `stop_date` as
+        ListDict.
 
         :param start_date: The beginning datetime record.
 
@@ -183,51 +197,53 @@ class VantagePro2:
         generator = self._get_archives_generator(start_date, stop_date)
         archives = ListDict()
         dates = []
-        async for item in generator:
+        for item in generator:
             if item["Datetime"] not in dates:
                 archives.append(item)
                 dates.append(item["Datetime"])
         return archives.sorted_by("Datetime")
 
-    async def _get_archives_generator(self, start_date=None, stop_date=None):
+    def _get_archives_generator(self, start_date=None, stop_date=None):
         """Get archive records generator until `start_date` and `stop_date`."""
-
-        await self.wake_up()
+        self.wake_up()
         # 2001-01-01 01:01:01
         start_date = start_date or datetime(2001, 1, 1, 1, 1, 1)
         stop_date = stop_date or datetime.now()
         # round start_date, with the archive period to the previous record
-        period = await self.archive_period  # pyright: ignore[reportGeneralTypeIssues]
+        period = self.archive_period
         minutes = start_date.minute % period
         start_date = start_date - timedelta(minutes=minutes)
-        await self.send("DMPAFT", self.ACK)
+        self.send("DMPAFT", self.ACK)
         # I think that date_time_crc is incorrect...
-        await self.link.write_async(await pack_dmp_date_time(start_date))
+        self.link.write(pack_dmp_date_time(start_date))
         # timeout must be at least 2 seconds
-        ack = await self.link.read_async(len(self.ACK))
+        ack = self.link.read(len(self.ACK), timeout=2)
         if ack != self.ACK:
-            raise BadAckException
+            raise BadAckException()
         # Read dump header and get number of pages
-        header = DmpHeaderParser(await self.link.read_async(6))
+        header = DmpHeaderParser(self.link.read(6))
         # Write ACK if crc is good. Else, send cancel.
         if header.crc_error:
-            await self.link.write_async(self.CANCEL.encode())
-            raise BadCRCException
+            self.link.write(self.CANCEL)
+            raise BadCRCException()
         else:
-            await self.link.write_async(self.ACK.encode())
+            self.link.write(self.ACK)
+        #        LOGGER.info('Starting download %d dump pages' % header['Pages'])
         finish = False
         r_index = 0
-        for _i in range(header["Pages"]):
+        for i in range(header["Pages"]):
             # Read one dump page
             try:
-                dump = await self._read_dump_page()
-            except (BadCRCException, BadDataException):
+                dump = self._read_dump_page()
+            except (BadCRCException, BadDataException) as e:
+                #                LOGGER.error('Error: %s' % e)
                 finish = True
                 break
+            #            LOGGER.info('Dump page no %d ' % dump['Index'])
             # Get the 5 raw records
             raw_records = dump["Records"]
             # loop through archive records
-            offsets = zip(range(0, 260, 52), range(52, 261, 52), strict=False)
+            offsets = zip(range(0, 260, 52), range(52, 261, 52))
             # offsets = [(0, 52), (52, 104), ... , (156, 208), (208, 260)]
             for offset in offsets:
                 raw_record = raw_records[offset[0] : offset[1]]
@@ -239,101 +255,106 @@ class VantagePro2:
                 # verify that record has valid data, and store
                 r_time = record["Datetime"]
                 if r_time is None:
+                    #                    LOGGER.error('Invalid record detected')
                     finish = True
                     break
                 elif r_time <= stop_date:
                     if start_date < r_time:
                         not_in_range = False
-                        msg = "Record-%.4d - Datetime : %s" % (r_index, r_time)  # noqa: UP031
+                        msg = "Record-%.4d - Datetime : %s" % (r_index, r_time)
+                        #                        LOGGER.info(msg)
                         yield record
                     else:
                         not_in_range = True
+                #                        LOGGER.info('The record is not in the datetime range')
                 else:
+                    #                    LOGGER.error('Invalid record detected')
                     finish = True
                     break
                 r_index += 1
             if finish:
-                await self.link.write_async(self.ESC.encode())
+                #                LOGGER.info('Canceling download : Finish')
+                self.link.write(self.ESC)
                 break
             elif not_in_range:
                 msg = "Page is not in the datetime range"
-                await self.link.write_async(self.ESC.encode())
+                #                LOGGER.info('Canceling download : %s' % msg)
+                self.link.write(self.ESC)
                 break
             else:
-                await self.link.write_async(self.ACK.encode())
+                #                if header['Pages'] - 1 == i:
+                #                    LOGGER.info('Start downloading next page')
+                self.link.write(self.ACK)
 
-    @async_cached_property
-    async def archive_period(self) -> int:
+    #        LOGGER.info('Pages Downloading process was finished')
+
+    @cached_property
+    def archive_period(self):
         """Returns number of minutes in the archive period."""
+        return struct.unpack(b"B", self.read_from_eeprom("2D", 1))[0]
 
-        return struct.unpack(b"B", await self.read_from_eeprom("2D", 1))[0]
-
-    @async_cached_property
-    async def timezone(self):
+    @cached_property
+    def timezone(self):
         """Returns timezone offset as string."""
-
-        data = await self.read_from_eeprom("14", 3)
+        data = self.read_from_eeprom("14", 3)
         offset, gmt = struct.unpack(b"HB", data)
         if gmt == 1:
             return "GMT+%.2f" % (offset / 100)
-        return "Localtime"
+        else:
+            return "Localtime"
 
-    @async_cached_property
-    async def firmware_date(self):
-        """Return the firmware date code."""
+    @cached_property
+    def firmware_date(self):
+        """Return the firmware date code"""
+        self.wake_up()
+        self.send("VER", self.OK)
+        data = self.link.read(13)
+        return datetime.strptime(data.strip("\n\r"), "%b %d %Y").date()
 
-        await self.wake_up()
-        await self.send("VER", self.OK)
-        data = await self.link.read_async(13)
-        return datetime.strptime(data.decode().strip("\n\r"), "%b %d %Y").date()
+    @cached_property
+    def firmware_version(self):
+        """Returns the firmware version as string"""
+        self.wake_up()
+        self.send("NVER", self.OK)
+        data = self.link.read(6)
+        return data.strip("\n\r")
 
-    @async_cached_property
-    async def firmware_version(self):
-        """Returns the firmware version as string."""
-
-        await self.wake_up()
-        await self.send("NVER", self.OK)
-        data = await self.link.read_async(6)
-        return data.decode().strip("\n\r")
-
-    @async_cached_property
-    async def diagnostics(self):
-        """Return the Console Diagnostics report. (RXCHECK command)."""
-
-        await self.wake_up()
-        await self.send("RXCHECK", self.OK)
-        data = await self.link.read_async(4048)
+    @cached_property
+    def diagnostics(self):
+        """Return the Console Diagnostics report. (RXCHECK command)"""
+        self.wake_up()
+        self.send("RXCHECK", self.OK)
+        data = self.link.read().strip("\n\r").split(" ")
         data = [int(i) for i in data]
-        return {
-            "total_received": data[0],
-            "total_missed": data[1],
-            "resyn": data[2],
-            "max_received": data[3],
-            "crc_errors": data[4],
-        }
+        return dict(
+            total_received=data[0],
+            total_missed=data[1],
+            resyn=data[2],
+            max_received=data[3],
+            crc_errors=data[4],
+        )
 
-    @retry(retry_policy)
-    async def _read_dump_page(self):
+    @retry(tries=3, delay=1)
+    def _read_dump_page(self):
         """Read, parse and check a DmpPage."""
-
-        raw_dump = await self.link.read_async(267)
+        raw_dump = self.link.read(267)
         if len(raw_dump) != 267:
-            await self.link.write_async(self.NACK.encode())
-            raise BadDataException
-        dump = DmpPageParser(raw_dump)
-        if dump.crc_error:
-            await self.link.write_async(self.NACK.encode())
-            raise BadCRCException
-        return dump
+            self.link.write(self.NACK)
+            raise BadDataException()
+        else:
+            dump = DmpPageParser(raw_dump)
+            if dump.crc_error:
+                self.link.write(self.NACK)
+                raise BadCRCException()
+            return dump
 
-    async def _check_revision(self):
+    def _check_revision(self):
         """Check firmware date and get data format revision."""
-
         # Rev "A" firmware, dated before April 24, 2002 uses the old format.
         # Rev "B" firmware dated on or after April 24, 2002
         date = datetime(2002, 4, 24).date()
         self.RevA = self.RevB = True
-        if await self.firmware_date < date:  # type: ignore  # noqa: PGH003
+        if self.firmware_date < date:
             self.RevB = False
         else:
             self.RevA = False
