32c92e6b9cbe567b9784098dce8cbb9cdc1135a5
moparman70 <58913167+moparman70@users.noreply.github.com>
2025-11-14 20:31:27 -0500

    Update logger.py

--- pyvantagepro3/logger.py
+++ pyvantagepro3/logger.py
@@ -1,31 +1,22 @@
-"""
-    pyvantagepro2.logger
-    -------------------
+"""pyvantagepro2.logger-------------------Logging setup."""
 
-    Logging setup.
-
-
-"""
-from __future__ import unicode_literals
 import logging
-from .compat import NullHandler
+from logging import NullHandler
 
-
-LOGGER = logging.getLogger('pyvpdriver')
+LOGGER = logging.getLogger("pyvpdriver")
 LOGGER.addHandler(NullHandler())
 
 
 def active_logger():
-    '''Initialize a speaking logger with stream handler (stderr).'''
-    LOGGER = logging.getLogger('pyvpdriver')
+    """Initialize a speaking logger with stream handler (stderr)."""
 
     LOGGER.setLevel(logging.INFO)
-    logging.getLogger('pylink').setLevel(logging.INFO)
+    logging.getLogger("pylink").setLevel(logging.INFO)
 
     # Default to logging to stderr.
-    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s ')
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s ")
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     LOGGER.addHandler(stream_handler)
-    logging.getLogger('pylink').addHandler(stream_handler)
+    logging.getLogger("pylink").addHandler(stream_handler)
