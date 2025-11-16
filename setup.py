dc03d37efc56ba33f2302d645eec9e3155c06231
moparman70 <58913167+moparman70@users.noreply.github.com>
2025-11-16 12:44:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='pyvantagepro3',
-    version='0.1.16',
+    version='0.1.17',
     url='https://github.com/moparman70/pyvantagepro2',
     license='GNU GPL v3',
     description='Communication tool for the Davis VP2',
